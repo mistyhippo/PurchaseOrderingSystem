7f19631486402a3544047fc1e0c86f06b5adf774
mistyhippo <32620699+mistyhippo@users.noreply.github.com>
2022-01-28 06:25:38 -0800

    Initial Commit project setup for project0

---
+++ src/main/java/com/revature/purchase/ordering/system/PurchaseOrderingDriver.java
@@ -0,0 +1,10 @@
+package com.revature.purchase.ordering.system;
+//import java.util.List;
+
+public class PurchaseOrderingDriver {
+
+    public static void main(String[] args){
+        // TODO document why this method is empty
+    }
+
+}
