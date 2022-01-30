24ed95aee4152b0b8052c911545223bb044f2c18
mistyhippo <32620699+mistyhippo@users.noreply.github.com>
2022-01-30 14:33:00 -0800

    setup product class

---
+++ src/main/java/com/revature/models/Product.java
@@ -0,0 +1,99 @@
+package com.revature.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Product {
+
+    private int productId;
+    private String productName;
+    private float price;
+    private String name;
+    private Person admin;
+    private List<Person> buyers;
+    private List<Person> sellers;
+
+    public Product(){
+        this.buyers = new ArrayList<>();
+        this.sellers = new ArrayList<>();
+    }
+
+    public Product(int productId, String productName, float price, String name, Person admin, List<Person> buyers, List<Person> sellers) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+        this.name = name;
+        this.admin = admin;
+        this.buyers = buyers;
+        this.sellers = sellers;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Person admin) {
+        this.admin = admin;
+    }
+
+    public List<Person> getBuyers() {
+        return buyers;
+    }
+
+    public void setBuyers(List<Person> buyers) {
+        this.buyers = buyers;
+    }
+
+    public List<Person> getSellers() {
+        return sellers;
+    }
+
+    public void setSellers(List<Person> sellers) {
+        this.sellers = sellers;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                ", name='" + name + '\'' +
+                ", admin=" + admin +
+                ", buyers=" + buyers +
+                ", sellers=" + sellers +
+                '}';
+    }
+}
