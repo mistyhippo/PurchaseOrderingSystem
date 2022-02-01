bb6d984bae86b7ad6416bcdd4bd650f6e054efec
mistyhippo <32620699+mistyhippo@users.noreply.github.com>
2022-02-01 05:27:44 -0800

    stashed changes

--- src/main/java/com/revature/PurchaseOrderingDriver.java
+++ src/main/java/com/revature/PurchaseOrderingDriver.java
@@ -1,10 +1,21 @@
 package com.revature;
 //import java.util.List;
 
+//import com.revature.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class PurchaseOrderingDriver {
 
     public static void main(String[] args){
-        // TODO document why this method is empty
+
+     /*   try{
+            Connection c = ConnectionUtil.getConnection();
+            System.out.println(c.getMetaData().getDriverName());
+        }catch (SQLException e){
+            e.printStackTrace();
+        }*/
     }
 
 }
---
+++ src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,13 @@
+package com.revature.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*public class ConnectionUtil {
+
+    public static Connection getConnection() throws SQLException {
+        DriverManager.getConnection(url, username,password);
+        return null;
+    }
+}*/
